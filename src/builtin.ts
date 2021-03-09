0581e73c5b536a2ac72d42701afb748798389fbc
zzGHzz <zzGHzz@github>
2021-03-09 11:11:27 +0800

    add solo mode default accounts

--- src/builtin.ts
+++ src/builtin.ts
@@ -18,6 +18,20 @@ const extensionABI = JSON.parse('[{"constant":true,"inputs":[],"name":"totalSupp
 const extensionV2ABI = JSON.parse('[{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"data","type":"bytes"}],"name":"blake2b256","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"num","type":"uint256"}],"name":"blockTime","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"num","type":"uint256"}],"name":"blockSigner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"num","type":"uint256"}],"name":"blockTotalScore","outputs":[{"name":"","type":"uint64"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"txGasPayer","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"txExpiration","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"txID","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"txProvedWork","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"num","type":"uint256"}],"name":"blockID","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"txBlockRef","outputs":[{"name":"","type":"bytes8"}],"payable":false,"stateMutability":"view","type":"function"}]');
 const prototypeABI = JSON.parse('[{"constant":false,"inputs":[{"name":"_self","type":"address"},{"name":"_newMaster","type":"address"}],"name":"setMaster","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_self","type":"address"},{"name":"_user","type":"address"}],"name":"isUser","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_self","type":"address"},{"name":"_key","type":"bytes32"}],"name":"storageFor","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_self","type":"address"},{"name":"_blockNumber","type":"uint256"}],"name":"energy","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_self","type":"address"},{"name":"_user","type":"address"}],"name":"removeUser","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_self","type":"address"}],"name":"currentSponsor","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_self","type":"address"},{"name":"_credit","type":"uint256"},{"name":"_recoveryRate","type":"uint256"}],"name":"setCreditPlan","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_self","type":"address"},{"name":"_sponsor","type":"address"}],"name":"selectSponsor","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_self","type":"address"},{"name":"_blockNumber","type":"uint256"}],"name":"balance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_self","type":"address"}],"name":"sponsor","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_self","type":"address"}],"name":"creditPlan","outputs":[{"name":"credit","type":"uint256"},{"name":"recoveryRate","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_self","type":"address"},{"name":"_user","type":"address"}],"name":"addUser","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_self","type":"address"}],"name":"hasCode","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_self","type":"address"}],"name":"master","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_self","type":"address"},{"name":"_user","type":"address"}],"name":"userCredit","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_self","type":"address"}],"name":"unsponsor","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_self","type":"address"},{"name":"_sponsor","type":"address"}],"name":"isSponsor","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"}]');
 
+// Solo mode accounts
+const soloAccounts: string[] = [
+	"0xdce1443bd2ef0c2631adc1c67e5c93f13dc23a41c18b536effbbdcbcdb96fb65",
+	"0x321d6443bc6177273b5abf54210fe806d451d6b7973bccc2384ef78bbcd0bf51",
+	"0x2d7c882bad2a01105e36dda3646693bc1aaaa45b0ed63fb0ce23c060294f3af2",
+	"0x593537225b037191d322c3b1df585fb1e5100811b71a6f7fc7e29cca1333483e",
+	"0xca7b25fc980c759df5f3ce17a3d881d6e19a38e651fc4315fc08917edab41058",
+	"0x88d2d80b12b92feaa0da6d62309463d20408157723f2d7e799b6a74ead9a673b",
+	"0xfbb9e7ba5fe9969a71c6599052237b91adeb1e5fc0c96727b66e56ff5d02f9d0",
+	"0x547fb081e73dc2e22b4aae5c60e2970b008ac4fc3073aebc27d41ace9c4f53e9",
+	"0xc8c53657e41a8d669349fc287f57457bd746cb1fcfc38cf94d235deb2cfca81b",
+	"0x87e0eba9c86c494d98353800571089f316740b0cb84c9a7cdf2fe5c9997c7966",
+]
+
 /**
  * Get the ABI for a specific function or event of a specific built-in contract
  * 
@@ -26,28 +40,29 @@ const prototypeABI = JSON.parse('[{"constant":false,"inputs":[{"name":"_self","t
  * @param type      - ('function' | 'event')
  */
 function getBuiltinABI(contract: string, name: string, type: 'function' | 'event'): object {
-    let abi: object[] = [];
-    switch (contract.toLowerCase()) {
-        case "prototype": { abi = prototypeABI; break; }
-        case "energy": { abi = energyABI; break; }
-        case "authority": { abi = authorityABI; break; }
-        case "extension": { abi = extensionABI; break; }
-        case "extensionv2": { abi = extensionV2ABI; break; }
-        case "params": { abi = paramsABI; break; }
-        case "executor": { abi = executorABI; break; }
-        default: { return {}; }
-    }
+	let abi: object[] = [];
+	switch (contract.toLowerCase()) {
+		case "prototype": { abi = prototypeABI; break; }
+		case "energy": { abi = energyABI; break; }
+		case "authority": { abi = authorityABI; break; }
+		case "extension": { abi = extensionABI; break; }
+		case "extensionv2": { abi = extensionV2ABI; break; }
+		case "params": { abi = paramsABI; break; }
+		case "executor": { abi = executorABI; break; }
+		default: { return {}; }
+	}
 
-    return getABI(abi, name, type);
+	return getABI(abi, name, type);
 }
 
 export {
-    authorityABI, authorityAddr,
-    energyABI, energyAddr,
-    paramsABI, paramsAddr,
-    extensionABI, extensionAddr,
-    extensionV2ABI, extensionV2Addr,
-    executorABI, executorAddr,
-    prototypeABI, prototypeAddr,
-    getBuiltinABI
+	authorityABI, authorityAddr,
+	energyABI, energyAddr,
+	paramsABI, paramsAddr,
+	extensionABI, extensionAddr,
+	extensionV2ABI, extensionV2Addr,
+	executorABI, executorAddr,
+	prototypeABI, prototypeAddr,
+	getBuiltinABI,
+	soloAccounts
 }
